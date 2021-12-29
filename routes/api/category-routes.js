e238b5f83ee3edcf142779f419a985d0a0a6f497
Janette <janette.castillo91@yahoo.com>
2021-12-28 19:29:38 -0600

    update category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -48,23 +48,25 @@ router.post('/', (req, res) => {
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
-  Category.update(
-    {
-      category_name: req.body.category_name,
-    },
-    {
-      where: {
-        id: req.params.id,
-      },
-    }
-  )
+    // update a category by its `id` value
+    try {
+      const categoryData = await Category.update({
+        category_name: req.body.category_name,
+        where: {
+          id: req.params.id,
+        }
+      });
 
-    .then((updatedCategory) => {
-      res.json(updatedCategory);
-    })
-    .catch((err) => res.json(err));
-});
+      if (!categoryData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(libraryCardData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
