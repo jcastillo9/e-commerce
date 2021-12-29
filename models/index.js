757dfb0c1f3b2337c8ac87679bf05d797ba093de
Janette <janette.castillo91@yahoo.com>
2021-12-28 23:52:32 -0600

    updated model index.js

--- models/index.js
+++ models/index.js
@@ -7,18 +7,21 @@ const ProductTag = require('./ProductTag');
 // Products belongsTo Category
 Product.belongsTo(Category, {
   foreignKey: 'category_id',
+  onDelete: 'CASCADE',
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id',
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_tag_id'
+  through: ProductTag,
+  foreignKey: 'product_id'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_tag_id'
+  through: ProductTag,
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
