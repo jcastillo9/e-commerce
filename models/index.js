f561670ec67500aa43f575b220e16f32dfffc5e5
Janette <janette.castillo91@yahoo.com>
2021-12-28 18:15:53 -0600

    create foreign key relationships

--- models/index.js
+++ models/index.js
@@ -4,6 +4,22 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+// Products belongsTo Category
+Product.belongsto(Category, {
+  foreignKey: 'category_id',
+});
+// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'product_id'
+});
+// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_tag_id'
+});
+// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_tag_id'
+});
 
 module.exports = {
   Product,
